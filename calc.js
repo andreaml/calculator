b011e9b3bfb35db3d888c3b637ae17af86fd5a8d
Andrea Muñoz <andreamunozliy@gmail.com>
2018-04-07 18:18:24 -0500

    Se agregan funciones para agregar operador a fórmula.

--- calc.js
+++ calc.js
@@ -8,8 +8,37 @@ function concatValue(newValue, element) {
     element.value += newValue;
 }
 
+function checkLastOperator(element, callback) {
+    let operators = {
+        "+": true,
+        "-": true,
+        "*": true,
+        "/": true
+    }
+    let lastValuePosition = element.value.length - 1;
+    let lastValue = element.value[lastValuePosition];
+    callback(operators[lastValue]);
+}
+
+function replaceLastOperator(newValue, element) {
+    let lastOperatorPosition = element.value.length - 1;
+    let arrayValues = element.value.split('');
+    arrayValues[lastOperatorPosition] = newValue;
+    element.value = arrayValues.join('');
+}
+
 function PutNumber(number) {
     concatValue(number, display);
+}
+
+function PutOperator(operator) {
+    checkLastOperator(display, function(validToReplace) {
+        if (validToReplace) {
+            replaceLastOperator(operator, display);
+        } else {
+            concatValue(operator, display);
+        }
+    });
 }
 
 function initCalculator() {

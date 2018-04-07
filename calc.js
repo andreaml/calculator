4a88796640a04045ffb19f9eb421c31530034076
Andrea Muñoz <andreamunozliy@gmail.com>
2018-04-07 17:28:01 -0500

    Se agrega función para poner número en display y concatenar valores.

--- calc.js
+++ calc.js
@@ -2,3 +2,18 @@ var NN=true;
 var Op="";
 var Num=0.0;
 var Mem=0.0;
+var display; 
+
+function concatValue(newValue, element) {
+    element.value += newValue;
+}
+
+function PutNumber(number) {
+    concatValue(number, display);
+}
+
+function initCalculator() {
+    display = document.getElementById("Display");
+}
+
+initCalculator();
